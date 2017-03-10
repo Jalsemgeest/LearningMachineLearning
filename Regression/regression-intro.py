9cd8ec9fa32766ad2614c555c5a0490d13caec94
Jake Alsemgeest <alsemgeester@gmail.com>
2017-03-09 20:16:26 -0500

    More final regression start.

--- Regression/regression-intro.py
+++ Regression/regression-intro.py
@@ -55,8 +55,7 @@ clf.fit(X_train, y_train)
 with open('linearregression.pickle', 'wb') as f:
   pickle.dump(clf, f)
 
-pickle_in = open('linearregression.pickle', 'rb')
-clf = pickle.load(pickle_in)
+
 # Score = test
 accuracy = clf.score(X_test, y_test)
 
